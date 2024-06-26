0197340d65368089ec55d83fa27234b1bde25768
Esau Jesse <jesseesau13@gmail.com>
2024-06-26 06:19:21 +0100

    Changed the the variable name 'operator' to 'operation'

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,17 +1,17 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-operator = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operator:
+match operation:
     case "+":
         print(num1 + num2)
     case "-":
         print(num1 - num2)
     case "*":
-        print(num1 + num2)
+        print(num1 * num2)
     case "/":
         if (num2 == 0):
-            print("Sorry, you can't divide by zero!")
+            print("Cannot divide by zero!")
         else:
             print(num1 / num2)
