8d4996f483be9dda1adf83f2f2cc4f591c59b755
Esau Jesse <jesseesau13@gmail.com>
2024-07-01 20:27:16 +0100

    Added to files, arithmetic_operations.py and main.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,27 @@
+def perform_operation(num1, num2, operation):
+
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("cant divide by zero")
+            else:
+                result = num1 / num2
+    return float(result)
+
+
+print(perform_operation(5, 6, "add"))
+
+#  if operation == "add":
+# result = num1 + num2
+# elif operation == "subtract":
+#  result = num1 - num2
+# elif operation == "multiply":
+#   result = num1 * num2
+# elif operation == "divide" and num2 != 0:
+#   result = num1 / num2
