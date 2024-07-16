64b290501b2a7140b975c0898d8df51916e9b2dd
Esau Jesse <jesseesau13@gmail.com>
2024-07-16 10:09:47 +0100

    Added polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,28 @@
+import math
+
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        result = self.length * self.width
+        return result
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        result = math.pi * self.radius**2
+        return result
